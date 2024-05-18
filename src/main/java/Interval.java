ff1bd87494a7469d69277b05ab0027889dec2d05
Maksim Bogdanov <aspid888@gmail.com>
2024-05-18 03:57:50 +0300

    ..

--- src/main/java/Interval.java
+++ src/main/java/Interval.java
@@ -1,36 +1,18 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Interval {
     Pair<UUID, UUID> low;
     Pair<UUID, UUID> high;
-
-    public Interval() {
-        this.low = null;
-        this.high = null;
-    }
-
-    public Interval(Pair<UUID, UUID> low, Pair<UUID, UUID> high) {
-        this.low = low;
-        this.high = high;
-    }
 
     public boolean isEmpty() {
         return low == null && high == null;
     }
 
-    public Pair<UUID, UUID> getLow() {
-        return this.low;
-    }
-
-    public Pair<UUID, UUID> getHigh() {
-        return this.high;
-    }
-
-    public void setLow(Pair<UUID, UUID> low) {
-        this.low = low;
-    }
-
-    public void setHigh(Pair<UUID, UUID> high) {
-        this.high = high;
-    }
 }
