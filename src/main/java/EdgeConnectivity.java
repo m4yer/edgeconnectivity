a5a15faf858a990a93b6d5301a20721cbbd17645
Maxim Bogdanov <mabogdanov@dev.vtb.ru>
2024-05-18 03:42:35 +0300

    ..

--- src/main/java/EdgeConnectivity.java
+++ src/main/java/EdgeConnectivity.java
@@ -1,11 +1,34 @@
+import com.mathsystem.api.graph.model.Edge;
 import com.mathsystem.api.graph.model.Graph;
 import com.mathsystem.domain.plugin.plugintype.GraphProperty;
 
 public class EdgeConnectivity implements GraphProperty {
-    
+
     @Override
     public boolean execute(Graph graph) {
-        return graph.getEdgeCount().equals(graph.getVertexCount());
+        var planarityTester = new PlanarityTester(graph.getVertices().keySet().stream().toList());
+
+        for (Edge edge : graph.getEdges()) {
+            planarityTester.addEdge(edge.getFromV(), edge.getToV());
+        }
+
+        // graph has to be not planar initially
+        if (planarityTester.isPlanar()) {
+            return false;
+        }
+
+        for (Edge edge : graph.getEdges()) {
+            planarityTester.removeEdge(edge.getFromV(), edge.getToV());
+
+            // check if graph is planar by deleting edge
+            if (planarityTester.isPlanar()) {
+                return true;
+            }
+
+            planarityTester.addEdge(edge.getFromV(), edge.getToV());
+        }
+
+        return false;
     }
 
 }
