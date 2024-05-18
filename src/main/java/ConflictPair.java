5229e857124d1ab8faa8b1e060b684f6b3329f1a
Maksim Bogdanov <aspid888@gmail.com>
2024-05-18 03:56:25 +0300

    ..

--- src/main/java/ConflictPair.java
+++ src/main/java/ConflictPair.java
@@ -1,31 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 class ConflictPair {
     Interval left;
     Interval right;
 
-    public ConflictPair(Interval left, Interval right) {
-        this.left = left;
-        this.right = right;
-    }
-
     public void swapIntervals() {
-        Interval tmp = this.left;
+        var tmp = this.left;
         this.left = this.right;
         this.right = tmp;
-    }
-
-    public Interval getLeft() {
-        return this.left;
-    }
-
-    public Interval getRight() {
-        return this.right;
-    }
-
-    public void setLeft(Interval left) {
-        this.left = left;
-    }
-
-    public void setRight(Interval right) {
-        this.right = right;
     }
 }
